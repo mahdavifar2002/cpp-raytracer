ecf370e499e4ff3915276f545327774212e174df
Ali Mahdavifar <mahdavifar2002@gmail.com>
2026-07-18 00:31:24 +0200

    Finish section 11.4 (Schlick Approximation)

--- material.h
+++ material.h
@@ -84,6 +84,13 @@ class dielectric : public material {
     // Refractive index in vacuum or air, or the ratio of the material's refractive index
     // over the refractive index of the enclosing media
     double refraction_index;
+
+    static double reflectance(double cosine, double refraction_index) {
+        // Use Schlick's approximation for reflectance.
+        auto r0 = (1 - refraction_index) / (1 + refraction_index);
+        r0 = r0*r0;
+        return r0 + (1-r0)*std::pow((1 - cosine), 5);
+    }
 };
 
 #endif
