ec2c385099f01a0efbf56ad86190663dc79ceaf7
Ali Mahdavifar <mahdavifar2002@gmail.com>
2026-07-17 19:15:32 +0200

    Finish section 6.7 (Common Constants and Utility Functions)
    Comment: I didn't perform the removals of headers and such, mentioned in the "Listing 25" until "Listing 29" code snippets, since they could arise red error assumptions in my IDE (VS Code). The code could still be compiled with no error, but the error assumptions in the editor was annoying.

--- main.cc
+++ main.cc
@@ -1,28 +1,19 @@
+#include "rtweekend.h"
+
 #include "color.h"
 #include "ray.h"
 #include "vec3.h"
 
+#include "hittable.h"
+#include "hitabble_list.h"
+#include "sphere.h"
+
 #include <iostream>
 
-double hit_sphere(const point3& center, double radius, const ray& r) {
-    vec3 oc = center - r.origin();
-    auto a = r.direction().length_squared();
-    auto h = dot(r.direction(), oc);
-    auto c = oc.length_squared() - radius*radius;
-    auto delta = h*h - a*c;
-    
-    if (delta < 0) {
-        return -1;
-    } else {
-        return (h - std::sqrt(delta)) / a;
-    }
-}
-
-color ray_color(const ray& r) {
-    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
-    if (t > 0.0) {
-        vec3 N = unit_vector(r.at(t) - point3(0, 0, -1));
-        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+color ray_color(const ray& r, hittable_list& world) {
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1, 1, 1));
     }
     
     vec3 unit_direction = unit_vector(r.direction());
@@ -40,6 +31,13 @@ int main() {
     // Calculate the image height, and ensure that it's at least 1.
     int image_height = int (image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
+
+    // World
+
+    hittable_list world;
+
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
     // Camera
 
@@ -78,7 +76,7 @@ int main() {
             auto ray_direction = pixel_center - camera_center;
             auto r = ray(camera_center, ray_direction);
 
-            auto pixel_color = ray_color(r);
+            auto pixel_color = ray_color(r, world);
             write_color(std::cout, pixel_color);
         }
     }
