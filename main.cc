be1e1991a911f740fc1506330292d67393697b62
Ali Mahdavifar <mahdavifar2002@gmail.com>
2026-07-18 01:26:41 +0200

    Finish section 12.2 (Positioning and Orienting the Camera)

--- main.cc
+++ main.cc
@@ -29,9 +29,14 @@ int main() {
     camera cam;
 
     cam.aspect_ratio     = 16.0 / 9.0;
-    cam.image_width      = 400;
+    cam.image_width      = 1920;
     cam.sample_per_pixel = 10;
     cam.max_depth        = 50;
+
+    cam.vfov = 20;
+    cam.lookfrom = point3(-2, 2, 1);
+    cam.lookat = point3(0, 0, -1);
+    cam.vup = vec3(0, 1, 0);
 
     cam.render(world);
 }
