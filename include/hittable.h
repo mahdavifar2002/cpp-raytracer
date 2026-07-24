78c1b2d9ed16bc3a709df158c3dfaa9623fbe371
Ali Mahdavifar <mahdavifar2002@gmail.com>
2026-07-24 23:34:35 +0200

    Finish section 4.1 (Constant Color Texture)

--- include/hittable.h
+++ include/hittable.h
@@ -11,13 +11,14 @@ class hit_record {
     point3 p;
     vec3 normal;
     shared_ptr<material> mat;
-    double t;
+    double t; // the parameteric value at the point which ray hits object
+    double u; // the `u` coordinate of the ray-object hit point
+    double v; // the `v` coordinate of the ray-object hit point
     bool front_face;
 
+    // Sets the hit record normal vector.
+    // NOTE: the parameter `outward_normal` is assumed to have unit length.
     void set_face_normal(const ray& r, const vec3& outward_normal) {
-        // Sets the hit record normal vector.
-        // NOTE: the parameter `outward_normal` is assumed to have unit length.
-        
         front_face = dot(r.direction(), outward_normal) < 0;
         normal = front_face ? outward_normal : -outward_normal;
     }
