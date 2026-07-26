941df866287b354779b82db9ab7ea1a48b884672
Ali Mahdavifar <mahdavifar2002@gmail.com>
2026-07-26 23:29:21 +0200

    Finish section 5.1 (Using Blocks of Random Numbers)

--- include/texture.h
+++ include/texture.h
@@ -2,6 +2,7 @@
 #define TEXTURE_H
 
 #include "color.h"
+#include "perlin.h"
 #include "rtw_stb_image.h"
 
 class texture {
@@ -71,6 +72,18 @@ class image_texture : public texture {
 
   private:
     rtw_image image;
+};
+
+class noise_texture : public texture {
+  public:
+    noise_texture() {}
+
+    color value(double u, double v, const point3& p) const override {
+        return color(1, 1, 1) * noise.noise(p);
+    }
+
+  private:
+    perlin noise;
 };
 
 #endif
